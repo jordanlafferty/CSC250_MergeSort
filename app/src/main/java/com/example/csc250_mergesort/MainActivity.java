825a2f5e87f73736ec73eee93b5b4c9b6f166ea8
Jordan Lafferty <jordanlafferty99@yahoo.com>
2021-04-12 11:48:35 -0500

    HW 16 Solution

--- app/src/main/java/com/example/csc250_mergesort/MainActivity.java
+++ app/src/main/java/com/example/csc250_mergesort/MainActivity.java
@@ -19,13 +19,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.inputET = this.findViewById(R.id.inputET);
         this.answerTV = this.findViewById(R.id.answerTV);
+
+
     }
+    private int factorialIter(int n)
+    {
+        int runningTotal = n;
+        for (int i =n-1; i>0; i--)
+        {
+            runningTotal *= i;
+        }
+        return runningTotal;
+    }
+
+    private int factorialRec(int n)
+    {
+        // also can be written as if (n== 1) return 1;
+        if (n== 1)
+        {
+            return 1;
+        }
+        return n* factorialRec(n-1);
+    }
+
+    private int factorialRec2(int n)
+    {
+        //shorter way to solve
+        // Inline if statement
+        //boolean_exp?true_exp;false_exp;
+        return n==1?1:n* factorialRec(n-1);
+    }
+
 
     public void onFactorialButtonClicked(View v)
     {
         String currValue = this.inputET.getText().toString();
         this.answerTV.setText(currValue);
-        int factorial = Integer.parseInt(currValue);
+        int currValueasInt = Integer.parseInt(currValue);
+        this.answerTV.setText(""+this.factorialIter(currValueasInt));
+
+        //how I solved it
+        /*int factorial = Integer.parseInt(currValue);
         int total = Integer.parseInt(currValue);
         int num = Integer.parseInt(currValue);
 
@@ -41,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 total = total * (factorial-1);
                 factorial--;
             }
-        }
+        }*/
 
     }
 
